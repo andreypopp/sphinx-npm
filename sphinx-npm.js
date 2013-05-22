0f9cbaba7633d0fcddbc37c61cd9b50d8cbb7fc4
Andrey Popp <8mayday@gmail.com>
2013-05-23 02:32:37 +0400

    fix handling of cmdargs

--- sphinx-npm.js
+++ sphinx-npm.js
@@ -3,6 +3,8 @@ var spawn = require('child_process').spawn,
 		join = require('path').join;
 
 exports.main = function (srcdir) {
+	var cmdArgs = Array.prototype.slice.call(arguments, 1);
+	cmdArgs.splice(cmdArgs.length - 1, 0, srcdir);
 	var pkg = require(join(srcdir,  "package.json")),
 			author = pkg.author.name || pkg.author,
 			project = pkg.name,
@@ -19,14 +21,17 @@ exports.main = function (srcdir) {
 				"-Dmaster_doc=README",
 				"-bsinglehtml",
 				"-C",
-			].concat(Array.prototype.slice.call(arguments, 0)),
+			].concat(cmdArgs),
 			ps = spawn("sphinx-build", args);
 
 	return ps;
 };
 
 if (require.main === module) {
-	var ps = exports.main.apply(null, process.argv.slice(2));
+	var args = process.argv.slice(2);
+	args.unshift(args.splice(args.length - 2, 1)[0]);
+
+	var ps = exports.main.apply(null, args);
 	ps.stdout.pipe(process.stdout);
 	ps.stderr.pipe(process.stderr);
 	ps.on('exit', function (code) { process.exit(code); });
